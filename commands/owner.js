a4bf33c33492538f2d9637f1b03f3ae96aba5350
RexUnderscore <166715321+RexUnderscore@users.noreply.github.com>
2024-04-11 21:44:38 +0700

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -34,7 +34,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`**What Are You Looking For🤣**`)
+            .setDescription(`**What Are You Looking For🤣 Happy Birthday BTW**`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
