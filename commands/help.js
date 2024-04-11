2b8997477bbf5fd7b4e38a92a4de1a3048e517f3
RexUnderscore <166715321+RexUnderscore@users.noreply.github.com>
2024-04-11 21:43:26 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -57,20 +57,20 @@ module.exports = {
            { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
           { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('&')
       const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setLabel('Happy')
+        .setURL('https://youtu.be/cSQTZoZPJzs?si=XTTIn76oK8LACSh1')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
-        .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setLabel('Birth')
+        .setURL('https://youtu.be/cSQTZoZPJzs?si=XTTIn76oK8LACSh1')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
+        .setLabel('Day')
+        .setURL('https://youtu.be/cSQTZoZPJzs?si=XTTIn76oK8LACSh1')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
